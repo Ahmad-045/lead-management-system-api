4efe4a8b283ced87ef0c6a4d107f2cfcafd6d157
Ahmad-045 <ahmad.amin@devsinc.com>
2022-11-10 19:05:01 +0500

    Added the Role and User validation for valid role

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,14 +1,23 @@
 class Role < ApplicationRecord
+  ROLES = ['guest', 'engineer', 'bd', 'manager', 'admin']
+
   has_and_belongs_to_many :users, :join_table => :users_roles
-  
+
   belongs_to :resource,
              :polymorphic => true,
              :optional => true
-  
+
 
   validates :resource_type,
             :inclusion => { :in => Rolify.resource_types },
             :allow_nil => true
 
   scopify
+
+  validate :test_valid_input_role
+
+  def test_valid_input_role
+    errors.add(:base, 'Invalid Role') unless ROLES.include? self.name
+  end
+
 end
