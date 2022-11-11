a7613a1fc3fa5af226e06243d14847b911b7f83a
Ahmad-045 <ahmad.amin@devsinc.com>
2022-11-11 13:32:54 +0500

    new fields to user and register with them

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # rescue_from ActiveRecord::RecordNotUnique, with: :record_not_unique
+
+  # private
+  # def record_not_unique
+  #   render json: {data: 'Record Not Unique', status: :conflict}
+  # end
   # protect_from_forgery with: :null_session
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:username, :email, :password, :password_confirmation, :contact)
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   #  :recoverable, :rememberable, :validatable
 
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :validatable,
   :jwt_authenticatable,
   jwt_revocation_strategy: JwtDenylist
 
